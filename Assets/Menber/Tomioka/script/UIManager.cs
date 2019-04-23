83e533eafc49b7cef232810cd9159c05d82a1a51
Tomioka <tyuuni31@gmail.com>
2019-04-23 18:45:21 +0900

    メニューのボタンとスクリプトを追加 sceneは未実装

---
+++ Assets/Menber/Tomioka/script/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void OnClickShopButton(){
+        SceneManager.LoadScene("");
+    }
+
+    public void OnClickResearchButton(){
+        SceneManager.LoadScene("");
+    }
+
+    public void OnClickCustomerStatusButton(){
+        SceneManager.LoadScene("");
+    }
+
+    public void OnClickStartAuctionButton(){
+        SceneManager.LoadScene("");
+    }
+}
