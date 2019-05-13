85f69c39a768b6523c79e9e6a012fbd7d24ee336
高田将志 <masapo.0315@icloud.com>
2019-05-13 23:45:13 +0900

    表向きの共通の上乗せ条件金額に達したら一旦上乗せを中断するようにした。上乗せする金額も10円単位でランダムに上げるようにした。

--- Assets/Menber/Takada/MoneyUp.cs
+++ Assets/Menber/Takada/MoneyUp.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class MoneyUp : MonoBehaviour
 {
+    //CPUが商品の金額をあげるスクリプト
+
     int UpMoney; // 上げる金額
     public int playerMoney; // NPCのマネー
-    int UpperLimit = 0; // 上げられる表向きの金額上限
-    public int OneUpLimit; //一度に挙げられる金額の上限 
+    public static int UpperLimit; // 上げられる表向きの金額上限
+    int OneUpLimit = 10; //一度に挙げられる金額の上限 
 
 
     // Start is called before the first frame update
@@ -25,12 +27,14 @@ public class MoneyUp : MonoBehaviour
     //金額の加算処理
     private void Addition(){
 
-        // 金額上限でなければ加算
-        if (UpperLimit < 1000 && playerMoney > 0){
+        // 手持ち金があり金額上限でなければ加算
+        if (UpperLimit < 1000 && playerMoney > 100){
 
-            UpperLimit += 100;
-            playerMoney -= 100;
+            UpMoney = 10 * Random.Range(1, OneUpLimit + 1);
+            UpperLimit += UpMoney;
+            playerMoney -= UpMoney;
             Debug.Log(transform.name + "残り金額" + playerMoney + "円です");
+            //Debug.Log(UpperLimit);
 
         }
         
