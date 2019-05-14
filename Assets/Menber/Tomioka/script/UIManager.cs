00a47795508202276d76f521f2285254a16f1c6d
tokyotech-hosyu <tokyotech-hosyu@4900.co.jp>
2019-05-14 19:34:56 +0900

    客と商品の情報を載せたウィンドウの情報を書き換えられるように変更

--- Assets/Menber/Tomioka/script/UIManager.cs
+++ Assets/Menber/Tomioka/script/UIManager.cs
@@ -8,7 +8,7 @@ public class UIManager : MonoBehaviour
 {
 
     [SerializeField]
-    private GameObject status;
+    private GameObject statusWindow;
     [SerializeField]
     private GameObject myMoney;
     [SerializeField]
@@ -28,27 +28,33 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private GameObject customer4;
 
-    void Start()
-    {
-        status.SetActive(false);
+    bool researchCustomer1 = false;
+    bool researchCustomer2 = false;
+    bool researchCustomer3 = false;
+    bool researchCustomer4 = false;
+
+    string customerName1 = "まれいたそ";
+    string customerLike1 = "水素水";
+    int product = 100;
+
+    void Start(){
+        statusWindow.SetActive(false);
+        ChangeText();
     }
 
     //ボタンでscene移動　※移動先のsceneは未制作※
 
-    public void OnClickShopButton()
-    {
+    public void OnClickShopButton(){
         SceneManager.LoadScene("");
         Debug.Log("shop画面は出ないはずだよ");
     }
 
-    public void OnClickResearchButton()
-    {
+    public void OnClickResearchButton(){
         SceneManager.LoadScene("");
         Debug.Log("research画面は出ないはずだよ");
     }
 
-    public void OnClickStartAuctionButton()
-    {
+    public void OnClickStartAuctionButton(){
         SceneManager.LoadScene("");
         Debug.Log("オークション画面は出ないはずだよ");
     }
@@ -70,16 +76,28 @@ public class UIManager : MonoBehaviour
     ///Dさん ？？？が好き
     ////// </summary>
 
-    public void OnClickCheckStatusButton()
-    {
-        status.SetActive(true);
+    public void OnClickCheckStatusButton(){
+        statusWindow.SetActive(true);
     }
 
-    public void OnClickButton()
-    {
-        status.SetActive(false);
+    public void OnClickButton(){
+        statusWindow.SetActive(false);
     }
 
+    void ChangeText(){
+        Text product1_Text = product1.GetComponent<Text>();
+        Text product2_Text = product2.GetComponent<Text>();
+        Text product3_Text = product3.GetComponent<Text>();
+        Text product4_Text = product4.GetComponent<Text>();
 
+        Text customer1_Text = customer1.GetComponent<Text>();
+        Text customer2_Text = customer2.GetComponent<Text>();
+        Text customer3_Text = customer3.GetComponent<Text>();
+        Text customer4_Text = customer4.GetComponent<Text>();
+
+
+        product1_Text.text = customerLike1 + "(" + product + "円)";
+        customer1_Text.text = customerName1 + "は" + customerLike1 + "が好きだ";
+    }
 
 }
