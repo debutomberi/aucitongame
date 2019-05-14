6bbcbfd27b98c85bef6f3dea82e4baeea4ad4cfe
Fujita <>
2019-05-14 19:15:52 +0900

    ボタンによる金額の上げ下げと入札時の合計金額をゲーム画面に表示

--- Assets/Menber/Fujita/SakuraManager.cs
+++ Assets/Menber/Fujita/SakuraManager.cs
@@ -5,17 +5,56 @@ using UnityEngine.UI;
 
 public class SakuraManager : MonoBehaviour
 {
+    [SerializeField]
+    public Text label;
+    public Text aaa;
+    [SerializeField]
     public int count;
+    [SerializeField]
+    public int bidcount;
+    
     //public int SakuraMoney;
 
+    
     void Start()
     {
+        bidcount = 0;
         count = 0;
-    //  SakuraMoney = 0;
+        UpdateLabel();
     }
 
-    public void Update()
+
+    //クリック時金額を上げる
+    public void UpClick()
     {
-        this.GetComponent<Text>().text = count.ToString() + "円";
+        count = count + 1000;
+        UpdateLabel();
+    }
+
+    //クリック時金額が0を下回らないように下げる
+    public void DownClic()
+    {
+        if (count == 0)
+        {
+            count = count + 1000;
+        }
+
+        count = count - 1000;
+        UpdateLabel();
+    }
+
+    //入札時元々の金額に足されるようにする
+    public void bidClick()
+    {
+        bidcount = bidcount + count;
+        //bidcount(合計金額の表示)
+        aaa.text = bidcount + "円";
+
+    }
+
+    public void UpdateLabel()
+    {
+        label.text = count + "円";
+        aaa.text = bidcount + "円";
     }
 }
