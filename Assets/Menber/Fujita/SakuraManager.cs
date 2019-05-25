fe3626ca25cd5b174335c89ec5ef888c1c13a0d4
ryusuke <ryusuke0428tora@gmail.com>
2019-05-25 23:46:31 +0900

    comが自動で金額を上げた後プレイヤーがボタンクリックで金額をあげられるようにした(画面に表示済み)

--- Assets/Menber/Fujita/SakuraManager.cs
+++ Assets/Menber/Fujita/SakuraManager.cs
@@ -5,56 +5,38 @@ using UnityEngine.UI;
 
 public class SakuraManager : MonoBehaviour
 {
+    public MoneyPlus moneyPlus;
     [SerializeField]
-    public Text label;
     public Text aaa;
-    [SerializeField]
-    public int count;
-    [SerializeField]
-    public int bidcount;
-    
-    //public int SakuraMoney;
 
-    
     void Start()
     {
-        bidcount = 0;
-        count = 0;
+        moneyPlus = GetComponent<MoneyPlus>();
         UpdateLabel();
     }
 
+    private void Update()
+    {
+        UpdateLabel();
+    }
 
     //クリック時金額を上げる
     public void UpClick()
     {
-        count = count + 1000;
-        UpdateLabel();
-    }
-
-    //クリック時金額が0を下回らないように下げる
-    public void DownClic()
-    {
-        if (count == 0)
-        {
-            count = count + 1000;
-        }
-
-        count = count - 1000;
+        int _ItemRate = moneyPlus._ItemRate + 500;
         UpdateLabel();
     }
 
     //入札時元々の金額に足されるようにする
     public void bidClick()
     {
-        bidcount = bidcount + count;
-        //bidcount(合計金額の表示)
-        aaa.text = bidcount + "円";
-
+        moneyPlus._ItemRate = moneyPlus._ItemRate + 500;
+        //_ItemRate(合計金額の表示)
+        aaa.text = moneyPlus._ItemRate + "円";
     }
-
+    
     public void UpdateLabel()
     {
-        label.text = count + "円";
-        aaa.text = bidcount + "円";
+        aaa.text = moneyPlus._ItemRate + "円";
     }
 }
