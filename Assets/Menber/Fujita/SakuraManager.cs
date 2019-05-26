1204588d93e51adba14e0930eb93d62647bfa384
ryusuke <ryusuke0428tora@gmail.com>
2019-05-26 23:11:06 +0900

    金額の上げ下げ入札ボタンを再度作り直し、入札する金額の表示と合計金額の表示

--- Assets/Menber/Fujita/SakuraManager.cs
+++ Assets/Menber/Fujita/SakuraManager.cs
@@ -5,38 +5,63 @@ using UnityEngine.UI;
 
 public class SakuraManager : MonoBehaviour
 {
-    public MoneyPlus moneyPlus;
     [SerializeField]
+    public Text label;
     public Text aaa;
+    [SerializeField]
+    public int count;
+    //[SerializeField]
+    //public int bidcount;
+
+    public MoneyPlus moneyPlus;
+    //public int SakuraMoney;
+
 
     void Start()
     {
+        //bidcount = 0;
         moneyPlus = GetComponent<MoneyPlus>();
+        count = 0;
         UpdateLabel();
     }
 
-    private void Update()
+    public void Update()
     {
         UpdateLabel();
     }
+
 
     //クリック時金額を上げる
     public void UpClick()
     {
-        int _ItemRate = moneyPlus._ItemRate + 500;
+        count = count + 100;
+        UpdateLabel();
+    }
+
+    //クリック時金額が0を下回らないように下げる
+    public void DownClic()
+    {
+        if (count == 0)
+        {
+            count = count + 100;
+        }
+
+        count = count - 100;
         UpdateLabel();
     }
 
     //入札時元々の金額に足されるようにする
     public void bidClick()
     {
-        moneyPlus._ItemRate = moneyPlus._ItemRate + 500;
-        //_ItemRate(合計金額の表示)
+        //bidcount = bidcount + count;
+        moneyPlus._ItemRate = moneyPlus._ItemRate + count;
+        //bidcount(合計金額の表示)
         aaa.text = moneyPlus._ItemRate + "円";
     }
-    
+
     public void UpdateLabel()
     {
+        label.text = count + "円";
         aaa.text = moneyPlus._ItemRate + "円";
     }
 }
