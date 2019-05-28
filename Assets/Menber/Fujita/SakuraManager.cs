5ca3db6eb3644e2c863ca9d9ffee5d8129f98de1
ryusuke <ryusuke0428tora@gmail.com>
2019-05-28 19:03:26 +0900

    仮のUI配置、プレイヤー入札額の機能をdelta.timeからcoroutineに変更予定

--- Assets/Menber/Fujita/SakuraManager.cs
+++ Assets/Menber/Fujita/SakuraManager.cs
@@ -6,12 +6,16 @@ using UnityEngine.UI;
 public class SakuraManager : MonoBehaviour
 {
     [SerializeField]
-    public Text label;
-    public Text aaa;
-    [SerializeField]
+    public Text Count;
+    public Text BidCount;
+
+    public Text PlayerCount;
+    public Text AI1;
+    public Text AI2;
+    public Text AI3;
+    public Text AI4;
     public int count;
-    //[SerializeField]
-    //public int bidcount;
+    //public float Timecount = 5;
 
     public MoneyPlus moneyPlus;
     //public int SakuraMoney;
@@ -21,12 +25,12 @@ public class SakuraManager : MonoBehaviour
     {
         //bidcount = 0;
         moneyPlus = GetComponent<MoneyPlus>();
-        count = 0;
         UpdateLabel();
     }
 
     public void Update()
     {
+       // Timecount -= Time.deltaTime;
         UpdateLabel();
     }
 
@@ -55,13 +59,22 @@ public class SakuraManager : MonoBehaviour
     {
         //bidcount = bidcount + count;
         moneyPlus._ItemRate = moneyPlus._ItemRate + count;
-        //bidcount(合計金額の表示)
-       // aaa.text = "現在の金額" + moneyPlus._ItemRate + "円";
+
+
+        //if (count >= 0 || Timecount == 0)
+        //{
+        //    Timecount = 5;
+        //    count = 0;
+        //}
     }
 
     public void UpdateLabel()
     {
-        label.text = "入札する金額\n" + count + "円";
-        aaa.text = "現在の金額" + moneyPlus._ItemRate + "円";
+        //count(入札金額の表示)
+        Count.text = "入札する金額\n" + count + "円";
+        //PlayerCount(プレイヤーの宣言)
+        PlayerCount.text = count + "";
+        //bidcount(合計金額の表示)
+        BidCount.text = "現在の金額" + moneyPlus._ItemRate + "円";
     }
 }
