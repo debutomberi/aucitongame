8722698eb131dda64d650aaa6cf82ef8967e2f0e
高田将志 <masapo.0315@icloud.com>
2019-06-09 00:53:53 +0900

    スクリプト製作途中

---
+++ Assets/Menber/Takada/Auction.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Auction : MonoBehaviour
+{
+    List<GameObject> cpus = new List<GameObject>();
+
+    int upMoney;
+    int uppedMoney;
+    int[] upperLimit = new int[4];
+    int oneUpLimit;
+
+    bool auctionStart = true;
+    bool plusFlag = true;
+
+    public int[] cpuMoney = new int[4];
+    public string[] cpuName = new string[4];
+
+    [SerializeField]
+    private float intaval;
+    private float tmpTime = 0;
+ 
+    int _itemRate;
+    int itemCount;
+    AitemType _itemType;
+    AitemType cpuFavorite;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AitemBox.Instance.AucitonStart())
+        {
+            if (auctionStart)
+            {
+                _itemRate = AitemBox.Instance.AuctionStartprice(itemCount);
+                _itemType = AitemBox.Instance.AucitionAitemType(itemCount);
+                for(int i = 0; i != cpus.Count; i++)
+                {
+                    cpuMoney[i] = UserBox.Instance.UserMoneyCheck(i);
+                    cpuName[i] = UserBox.Instance.UserNameCheck(i);
+                }
+
+            }
+
+        }
+    }
+    
+    private void Addition()
+    {
+
+    }
+}
--- Assets/Menber/Takada/MoneyPlus.cs
+++ Assets/Menber/Takada/MoneyPlus.cs
@@ -9,22 +9,22 @@ public class MoneyPlus : MonoBehaviour
 
     public SucccesfulBid succcesfulBid;
 
-    public List<GameObject> CPUs = new List<GameObject>(); //CPUを入れるList
+    public GameObject[] CPUs = new GameObject[4]; //CPUを入れる配列
     int UpMoney; // 上乗せする金額
     int UppedMoney; //どれだけ上がったか
-    int UpperLimit; // 上乗せできる表向きの合計金額上限
+    int[] UpperLimit = new int[4]; // 上乗せできる表向きの合計金額上限
     int OneUpLimit = 10; //一度に上乗せできる金額の上限 
 
     bool AuctionStart = true; //オークション開始のフラグ
     bool plusFlag　= true;    //CPUが競りに参加しているかのフラグ
 
-    public int _cpuMoney; //CPUの所持金
+    public int[] _cpuMoney = new int[4]; //CPUの所持金
 
     [SerializeField]
     private float Intaval;　　//どれだけ遅延させるか
     private float tmpTime = 0;
 
-    public string _UserName;
+    public string[] _UserName = new string [4];
     public int _ItemRate;     //商品の初期金額
     AitemType _aitemType;     //商品の部類
     AitemType cpuFavorite;　　//お気に入りの商品
@@ -51,13 +51,12 @@ public class MoneyPlus : MonoBehaviour
                 _aitemType = AitemBox.Instance.AucitionAitemType(_ItemCount);
                 
 
-                for (int i = 0; i != CPUs.Count; i++)
+                for (int i = 0; i != CPUs.Length; i++)
                 {
-                    _cpuMoney = UserBox.Instance.UserMoneyCheck(i);
-                    _UserName = UserBox.Instance.UserNameCheck(i);
-                    _ItemRate = 100;
-                    UpperLimit = _ItemRate + 2000 + 10 * Random.Range(50, 200);
-                    if (UpperLimit > _cpuMoney)
+                    _cpuMoney[i] = UserBox.Instance.UserMoneyCheck(i);
+                    _UserName[i] = UserBox.Instance.UserNameCheck(i);
+                    UpperLimit[i] = _ItemRate + 2000 + 10 * Random.Range(50, 200);
+                    if (UpperLimit[i] > _cpuMoney[i])
                     {
                         UpperLimit = _cpuMoney;
                     }
@@ -87,19 +86,21 @@ public class MoneyPlus : MonoBehaviour
     //金額の上乗せ処理
     private void Addition()
     {
-        for (int i = 0; i < CPUs.Count; i++)
+        for (int i = 0; i < CPUs.Length; i++)
         {
-            _UserName = UserBox.Instance.UserNameCheck(i);
+            _UserName[i] = UserBox.Instance.UserNameCheck(i);
             cpuFavorite = UserBox.Instance.UserLikeCheck(i);
 
             // 金額上限でなければ上乗せする
             if (plusFlag)
             {
-                if (UppedMoney <= UpperLimit)
+                if (UppedMoney <= UpperLimit[i])
                 {
                     UpMoney = 10 * Random.Range(1, OneUpLimit + 1);
                     UppedMoney += UpMoney;
                     _ItemRate = _ItemRate + UppedMoney;
+                    //ここで誰が上げたか判定
+
                     Debug.Log(UppedMoney);
                     Debug.Log("現在の値段は" + _ItemRate + "円です");
                     Debug.Log(_UserName + "が掛け金を上乗せしました");
@@ -108,10 +109,10 @@ public class MoneyPlus : MonoBehaviour
 
                 else
 
-                if (UppedMoney >= UpperLimit && cpuFavorite == _aitemType)
+                if (UppedMoney >= UpperLimit[i] && cpuFavorite == _aitemType)
                 {
                     //好きな部類の商品なら上限を加算
-                    UpperLimit += 2000;
+                    UpperLimit[i] += 2000;
                     Debug.Log(_UserName + "の好きな部類の商品です");
 
                 }
@@ -125,7 +126,7 @@ public class MoneyPlus : MonoBehaviour
         
             //1人以外全員加算できなくなっていたら落札
 
-            for (int i = 0; i != CPUs.Count; i++)
+            for (int i = 0; i != CPUs.Length; i++)
             {
                 if (!CPUs[i]) { return; }
             }
